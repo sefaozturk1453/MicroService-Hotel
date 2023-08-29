51b2cdccc87a026cbbc74904c9d16989abe5a09e
sefaozturk1453 <sefaozturk1992@hotmail.com>
2023-08-29 23:10:44 +0300

    Test added

---
+++ Hotel.Test/StringHelperTests.cs
@@ -0,0 +1,35 @@
+﻿using Hotel.Helpers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Test
+{
+    public class StringHelperTests
+    {
+
+        [Test]
+        public void Bosluklar_Silindi_Mi()
+        {
+            // Arrenge
+
+            var str = "Muhammed     Ali";
+            var beklenen = "Muhammed Ali";
+
+
+            // Action
+
+            var gerceklesen = StringHelper.BosluklariSil(str);
+
+
+
+            // Assert
+
+            Assert.AreEqual(beklenen, gerceklesen);
+
+        }
+    }
+}
---
+++ Hotel.Test/TypeHelperTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hotel.Helpers;
+
+namespace Hotel.Test
+{
+    public class TypeHelperTests
+    {
+
+        [Test]
+        public void enum_Kontrol_Edildi_mi()
+        {
+            // Arrenge
+
+            var sayi = 2;
+            var beklenen = true;
+
+
+            // Action
+
+            var gerceklesen = TypeHelper.InfoTypeCheck(sayi);
+
+
+
+            // Assert
+
+            Assert.AreEqual(beklenen, gerceklesen);
+
+        }
+    }
+}
